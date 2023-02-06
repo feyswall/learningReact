eded575b03867a1243125064323906023a3c33a5
feyswall <vicentmkota91@gmail.com>
2023-02-06 10:29:19 +0300

    implement list in react

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,29 +1,40 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        counter: 0,
-        image: "https://picsum.photos/500"
-    }
-    render() {
-        let classes = "text-";
-        classes += this.state.counter == 0 ? "warning" : "success";
-        return (
-        <React.Fragment>
-                <div className="container m-4">
-                    <span className={classes}>
-                        {this.formatCount()}
-                </span>
-                <button className="btn btn-primary">increment</button>
+  state = {
+    counter: 1,
+    image: "https://picsum.photos/500",
+    tags: ["tag1", "tag2", "tag3"],
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container m-4">
+          <span className={this.getBadgeClasses()}>
+            <b>{this.formatCount()}</b>
+          </span>
+          <button className="btn btn-primary">increment</button>
+        <ul>
+                    {this.state.tags.map( tag => {
+                        return <li key={tag}>{tag}</li>
+                    })}           
+        </ul>
             </div>
-        </React.Fragment>
-        );
-    }
+        
+      </React.Fragment>
+    );
+  }
 
-    formatCount() {
-        let { counter } = this.state;
-        return counter === 0 ? "Zero" : counter;
-    }
+  getBadgeClasses() {
+    let classes = "p-4 text-";
+    classes += this.state.counter == 0 ? "warning" : "success";
+    return classes;
+  }
+
+  formatCount() {
+    let { counter } = this.state;
+    return counter === 0 ? "Zero" : counter;
+  }
 }
 
 export default Counter;
