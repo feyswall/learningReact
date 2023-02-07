6829fd354d759cc925357d01502a64cdde9e3df6
feyswall <vicentmkota91@gmail.com>
2023-02-07 21:51:27 +0300

    stateless functional components

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 
-class Navigation extends Component {
-    render() { 
+// stateless Functional conponent
+const Navigation = (props) => {
         return (
-       <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar { this.props.count}</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            </div>
-        </div>
-        </nav>
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid">
+                            <a class="navbar-brand" href="#">Navbar { props.count}</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    </div>
+                </div>
+            </nav>
         );
-    }
 }
+
 export default Navigation;
